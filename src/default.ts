5f1110ff952b2f7186cb74650ee450a31711fbe9
MmYYAIS <39291075+MmYYAIS@users.noreply.github.com>
2023-03-08 14:52:05 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,6 +8,6 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - Based on OpenAI API (gpt-3.5-turbo);
-- 设置里面填入自己的Openai API key 才可使用；
+- 设置里面填入自己的Openai API Key 才可使用；
 - 定义角色也在设置里面；
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
